f53486830ee28b040a12ca3b64fa6a1bc79c5fda
Lahiru Dilruwan <164044b@uom.lk>
2021-01-25 21:33:05 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'
+import Row from './Component/Row';
+import Requests from './Requests';
+import Banner from './Component/Banner';
+import Nav from './Component/Nav';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Nav/>
+      <Banner/>
+      <Row 
+         title="NETFLIX ORIGINAL"  
+         fetchUrl={Requests.fetchNetflixOriginals}
+         isLargeRow ={true}
+         ></Row>
+      <Row title="Trending now"  fetchUrl={Requests.fetchTrending}></Row> 
+      <Row title="Top Rated"  fetchUrl={Requests.fetchTopRated}></Row>
+      <Row title="Action Movies"  fetchUrl={Requests.fetchActionMovies}></Row>
+      <Row title="Comedy Movies"  fetchUrl={Requests.fetchComedyMovies}></Row>
+      <Row title="Horror Movies"  fetchUrl={Requests.fetchHorrorMovies}></Row>
+      <Row title="Romance Movies"  fetchUrl={Requests.fetchRomanceMovies}></Row>
+      <Row title="Documentaries"  fetchUrl={Requests.fetchDocumentaries}></Row>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Component/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Nav.css';
+
+function Nav() {
+    return (
+        <div className="nav">
+            <img
+             className="nav_logo"
+             src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1198px-Netflix_2015_logo.svg.png"
+             alt="Netflix Logo"
+            />
+        </div>
+    )
+}
+
+export default Nav
